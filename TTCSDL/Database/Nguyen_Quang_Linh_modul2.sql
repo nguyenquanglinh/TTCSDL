6c6295fc39d17977221d44fb2603b45c66ff3c9b
quang <quang@gmail.com>
2020-06-16 23:58:39 +0700

    hoàn thành bài mai đi thi

---
+++ TTCSDL/Database/Nguyen_Quang_Linh_modul2.sql
@@ -0,0 +1,167 @@
+﻿--1.Nhập các bảng danh mục (mỗi table 5 bản ghi) 
+--1.1.Thêm dữ liệu vào bảng Học kỳ,gia trị tương ứng là mã học kỳ, thời gian bắt đầu, thời gian kết thúc
+INSERT into HocKy ( IDHocKy, ThoiGianBatDau, ThoiGianKetThuc)
+values
+('6','20100813','20101224'),
+('7','20090115','20090511'),
+('8','20080813','20081224'),
+('9','20070115','20070511'),
+('10','20060813','20061224')
+GO
+select * from HocKy
+
+
+--1.2.Thêm dữ liệu vào bảng khoa , giá trị tương ứng là mã khoa và tên khoa
+INSERT Khoa (IDKhoa, TenKhoa) 
+VALUES (N'CK', N'Cơ khí'),
+(N'CĐ', N'Cầu đường'),
+(N'XD', N'Xây dựng'),
+(N'KT', N'Kinh tế'),
+(N'KD', N'Kinh doanh')
+GO
+select * from Khoa
+
+
+--1.3.Thêm dữ liệu vào bảng giảng viên, giá trị tương ứng là mã gv, tên gv và mã khoa
+INSERT GiangVien ([IDGV], [TenGV], [IDKhoa]) 
+VALUES 
+(N'GV07', N'Hoàng Trung Thông', N'XD'),
+(N'GV08', N'Ngô Thị Linh', N'XD'),
+(N'GV09', N'Hoàng Thái Bảo', N'KT'),
+(N'GV10', N'Chu Tiến Anh', N'KD'),
+(N'GV06', N'Phan Văn Đạt', N'CK')
+
+GO
+select * from GiangVien
+
+
+--1.4.Thêm dữ liệu vào bảng Lớp, giá trị tương ứng là mã lớp, tên lớp, sĩ số, niên khóa, mã khoa
+INSERT Lop (IDLop, TenLop, SiSo, NienKhoa, IDKhoa) 
+VALUES 
+(N'CĐ15', N'Cầu đường 15', NULL, N'2016-2021', N'CĐ'),
+(N'XD15', N'Xây dựng 15', NULL, N'2016-2021', N'XD'),
+(N'KD16', N'Kinh doanh 16', NULL, N'2017-2022', N'KD'),
+(N'KT', N'Kinh tế 16', NULL, N'2017-2022', N'KT'),
+(N'CK16', N'Cơ khí', NULL, N'2016-2021', N'CK')
+GO
+select * from Lop
+
+
+--1.5.Thêm dữ liệu vào bảng Sinh viên, giá trị tương ứng là mã sinh viên, họ tên, ngày sinh, giới tính, quê, địa chỉ,
+-- khoa đkí, số tín chỉ đã đạt, số tc đã đki, điểm tích lũy, mã lớp
+INSERT SinhVien (IDSinhVien, HoTen, NgaySinh, GioiTinh, QueQuan, DiaChiHT, KhoaDKi,  IDLop) 
+VALUES
+(N'17150140', N'Mai Kim Anh', CAST(N'1997-01-29' AS Date), N'Nữ', N'Nam Định', N'KTX Mỹ Đình', N'CDT',  NULL),
+(N'17150141', N'Lê Thu Thi Hương', CAST(N'1997-12-12' AS Date), N'Nữ', N'Phú Thọ', N'Trần Cung', N'CNTT',  NULL),
+(N'17150142', N'Ngô Quốc Phong', CAST(N'1997-07-01' AS Date), N'Nam', N'Yên Bái', N'Trần Cung', N'DTVT',  NULL),
+(N'17150143', N'Liên Doãn Bình', CAST(N'1997-04-02' AS Date), N'Nam', N'Nam Định', N'Trần Cung', N'KHMT',  NULL),
+(N'17150144', N'Nguyễn Thị Hương Giang', CAST(N'1997-07-01' AS Date), N'Nữ', N'Văn Chấn', N'KTX Mỹ Đình', N'DKTDH',  NULL)
+GO
+select * from sinhvien
+
+
+--1.6.Thêm dữ liệu vào bảng Học phần, giá trị tương ứng là mã môn học, tên môn học, số tiết, số tín chỉ, hình thức thi
+INSERT HocPhan (IDHocPhan, TenHocPhan, SoTiet, SoTC, HinhThucThi) 
+VALUES 
+(N'MH06', N'Hiểu biết quân binh chủng', 70, 3, N'Viết'),
+(N'MH07', N'Hình họa và BTL', 66, 3, N'Vấn Ðáp'),
+(N'MH08', N'Vẽ kỹ thuật & BTL', 80, 3, N'Vấn Đáp'),
+(N'MH09', N'Thị trường kinh doanh', 80, 4, N'Viết'),
+(N'MH010', N'Cấu trúc cơ mạch', 80, 4, N'Viết')
+GO
+SELECT * FROM HocPhan
+
+
+--1.7.Thêm dữ liệu vào bảng Lớp học phần, giá trị tương ứng là mã lớp hp, phòng học,số sv,ngày thi, mã hp, mã gv, mã hk
+INSERT LopHP (IDLopHP, PhongHoc, NgayThi, TongSoSV, IDHocPhan, IDHocKy, IDGV) 
+VALUES 
+(N'LHP06', N'H5401', '20191224', NULL, N'MH06', N'1', N'GV06'),
+(N'LHP07', N'H5501', '20191224', NULL, N'MH07', N'2', N'GV07'),
+(N'LHP08', N'H5502', '20191224', NULL, N'MH08', N'3', N'GV08'),
+(N'LHP09', N'H5411', '20191224', NULL, N'MH09', N'4', N'GV09'),
+(N'LHP010', N'H9201', '20191224', NULL, N'MH010', N'5', N'GV10')
+GO
+SELECT * FROM LopHP
+
+
+--1.8. Thêm dữ liệu vào bảo kết quả lớp học phần, giá trị tương ứng là mã sinh viên, mã lớp học phần, điểm chuyên cần
+-- điểm thường xuyên, điểm thi, điểm hệ 10, điểm hệ 4, điểm chữ
+INSERT KetQuaLopHP (IDSinhVien, IDLopHP, DiemCC, DiemTX, DiemThi, DiemHe10, DiemHe4, DiemChu) 
+VALUES 
+(N'17150140', N'LHP06', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150141', N'LHP07', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150142', N'LHP08', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150143', N'LHP09', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150144', N'LHP010', NULL, NULL, NULL, NULL, NULL, NULL)
+GO
+SELECT * FROM KetQuaLopHP
+
+
+--1.9. Thêm dữ liệu vào Tổng kết kỳ, giá trị tương ứng là mã sinh viên, mã học kỳ, 
+--điểm tb chung, số tín chỉ đăng ký, số tc nợ, xếp loại
+INSERT TongKetKy (IDSinhVien, IDHocKy, DiemTBC, SoTCDKi, SoTCNo, XepLoai) 
+VALUES 
+(N'17150140', N'1', NULL, NULL, NULL, NULL),
+(N'17150141', N'2', NULL, NULL, NULL, NULL),
+(N'17150142', N'3', NULL, NULL, NULL, NULL),
+(N'17150143', N'4', NULL, NULL, NULL, NULL),
+(N'17150144', N'5', NULL, NULL, NULL, NULL)
+GO
+SELECT * FROM TongKetKy
+
+
+--1.10. Thêm dữ liệu vào bảo kết quả lớp học phần, giá trị tương ứng là mã sinh viên, mã lớp học phần, điểm chuyên cần
+-- điểm thường xuyên, điểm thi, điểm hệ 10, điểm hệ 4, điểm chữ
+INSERT KetQuaHP (IDSinhVien, IDHocPhan, DiemCC, DiemTX, DiemThi, DiemHe10, DiemHe4, DiemChu) 
+VALUES 
+(N'17150140', N'MH06', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150141', N'MH07', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150142', N'MH08', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150143', N'MH09', NULL, NULL, NULL, NULL, NULL, NULL),
+(N'17150144', N'MH010', NULL, NULL, NULL, NULL, NULL, NULL)
+GO
+SELECT * FROM KetQuaHP
+
+--2.Nhập theo quy trình nghiệp vụ (3 bản ghi)
+--2.1 Thêm dữ liệu vào bảng sinh viên
+INSERT SinhVien (IDSinhVien, HoTen, NgaySinh, GioiTinh, QueQuan, DiaChiHT, KhoaDKi,  IDLop) 
+VALUES
+(N'17150145', N'Nguyễn Đức Mạnh', CAST(N'1999-01-29' AS Date), N'Nam', N'Nam Định', N'KTX Mỹ Đình', N'CDT',  NULL),
+(N'17150146', N'Vũ Thị Hường', CAST(N'1999-12-12' AS Date), N'Nữ', N'Phú Thọ', N'Trần Cung', N'CNTT',  NULL),
+(N'17150147', N'Nguyễn Thị Ngân', CAST(N'1999-07-01' AS Date), N'Nữ', N'Yên Bái', N'Trần Cung', N'DTVT',  NULL),
+(N'17150148', N'Mai Kim Tuyến', CAST(N'1999-04-02' AS Date), N'Nữ', N'Nam Định', N'Trần Cung', N'KHMT',  NULL),
+(N'17150149', N'Nguyễn Văn Duy', CAST(N'1999-07-01' AS Date), N'Nam', N'Văn Chấn', N'KTX Mỹ Đình', N'DKTDH',  NULL)
+GO
+select * from sinhvien
+--2.2 Thêm dữ liệu vào bảng sinh lớp(chuyên ngành)
+INSERT Lop (IDLop, TenLop, SiSo, NienKhoa, IDKhoa) 
+VALUES 
+(N'CĐ16', N'Cầu đường 16', NULL, N'2017-2022', N'CĐ'),
+(N'XD16', N'Xây dựng 16', NULL, N'2017-2022', N'XD'),
+(N'KD17', N'Kinh doanh 17', NULL, N'2018-2023', N'KD'),
+(N'KT18', N'Kinh tế 18', NULL, N'2018-2023', N'KT'),
+(N'CK18', N'Cơ khí 18', NULL, N'2018-2023', N'CK')
+GO
+select * from Lop
+--2.3 Thêm dữ liệu vào bảng sinh lớp(học phần)
+INSERT LopHP (IDLopHP, PhongHoc, NgayThi, TongSoSV, IDHocPhan, IDHocKy, IDGV) 
+VALUES 
+(N'LHP006', N'H1401', '20191224', NULL, N'MH06', N'1', N'GV06'),
+(N'LHP007', N'H1501', '20191224', NULL, N'MH07', N'2', N'GV07'),
+(N'LHP008', N'H1502', '20191224', NULL, N'MH08', N'3', N'GV08'),
+(N'LHP009', N'H1411', '20191224', NULL, N'MH09', N'4', N'GV09'),
+(N'LHP0010', N'H1201', '20191224', NULL, N'MH010', N'5', N'GV10')
+GO
+select * from LopHP
+--3.4cập nhật sinh ghi chú cho tất cả sinh viên
+begin
+update SinhVien set GhiChu = N'Không có gì' where GhiChu is null
+end
+go
+select * from sinhvien
+--2.5 Thêm cột số tc đã đạt
+ALTER table SinhVien add SoTCDaDat int
+--2.6 Thêm cột số tc đã đky 
+ALTER table SinhVien add SoTCDaDKi int
+--2.7 Thêm cột số tín chỉ tích lũy
+ALTER table SinhVien add SoTCTichLuy int
---
+++ TTCSDL/Database/Nguyễn-Quang-Linh_Modul3.sql
@@ -0,0 +1,276 @@
+﻿--1.1lấy ra danh sách thông tin  sinh viên của 1 lớp chuyên ngành nào đó
+	Select count(IDSinhVien) as STT,IDSinhVien as MaSv,HoTen ,NgaySinh,GioiTinh,QueQuan
+	from SinhVien where IDLop='TDH16     '
+	group by IDSinhVien,HoTen,NgaySinh,GioiTinh,QueQuan
+	ORDER BY  HoTen 
+	go
+	select * from SinhVien
+--1.2lấy ra danh sách kết quả sinh viên của 1 lớp học phần nào đó
+	Select count(SinhVien.IDSinhVien) as STT,SinhVien.IDSinhVien as 'Mã sinh viêm',HoTen ,TenHocPhan,DiemCC as ChuyenCan ,DiemTX as ThuongXuyen, DiemThi as CuoiKy
+	from SinhVien,HocPhan,KetQuaHP where SinhVien.IDSinhVien=KetQuaHP.IDSinhVien and KetQuaHP.IDHocPhan=HocPhan.IDHocPhan and  HocPhan.IDHocPhan='MH01      '
+	group by SinhVien.IDSinhVien,HoTen,NgaySinh,TenHocPhan,DiemCC,DiemTX,DiemThi
+	ORDER BY  HoTen 
+	go
+	select * from KetQuaLopHP
+--1.3Lấy ra ds kết quả kết quả sinh viên trong 1 lớp chuyên ngành(Điện tử viễn thông 16),khoa nào dó()
+	Select count(SinhVien.IDSinhVien) as STT,SinhVien.IDSinhVien as MaSv,HoTen ,TenLop,TenKhoa ,DiemTBC as DiemTrungBinhChung, SoTCDKi as SoTinChiDangKy,SoTCNo as SoTinChiNo,XepLoai
+	from SinhVien,Khoa,Lop,TongKetKy
+	 where SinhVien.IDSinhVien=TongKetKy.IDSinhVien and SinhVien.IDLop=Lop.IDLop and Lop.IDKhoa=Khoa.IDKhoa and  Lop.TenLop=N'Điện tử viễn thông 16'
+	group by SinhVien.IDSinhVien,HoTen,TenLop,TenKhoa,DiemTBC,SoTCDKi,SoTCNo,XepLoai
+	ORDER BY  HoTen 
+
+
+--2.1.đưa ra top 3 sinh viên có điểm cao nhất của lớp học phần thực thực tập cơ sở dữ liệu
+	--b1.Tạo view hiện thị danh sách kết quả của của các lớp học phần
+		go
+		CREATE VIEW DanhSachSinhVienCoDiemCaoNhatTrongLopHocPhan AS
+		select SinhVien.HoTen,SinhVien.IDSinhVien ,TenHocPhan  ,KetQuaLopHP.DiemHe10
+		from SinhVien,LopHP,KetQuaLopHP,HocPhan
+		where SinhVien.IDSinhVien=KetQuaLopHP.IDSinhVien and LopHP.IDLopHP=KetQuaLopHP.IDLopHP and HocPhan.IDHocPhan=LopHP.IDHocPhan
+		group by  SinhVien.HoTen,SinhVien.IDSinhVien,KetQuaLopHP.DiemHe10,TenHocPhan
+		go
+	--b2.Lấy ra 3 sinh viên điểm cao nhất
+		go
+		select top(3) HoTen as 'Họ và tên',IDSinhVien as 'Mã sinh viên',DiemHe10 as 'Điểm hệ số 10'
+		from DanhSachSinhVienCoDiemCaoNhatTrongLopHocPhan
+		where TenHocPhan = N'Cơ sở dữ liệu'
+		go
+--2.2(view) .đưa ra top 3 sinh viên có điểm cao nhất của lớp chuyên ngành cntt
+	--b1.Tạo view hiện thị danh sách kết quả của các lớp học chuyên ngành
+		go
+		CREATE VIEW DanhSachSinhVienCoDiemCaoNhatTrongLopChuyenNganh AS
+		select SinhVien.HoTen ,SinhVien.IDSinhVien,TenLop  ,DiemTBC,SoTCDKi,SoTCNo
+		from SinhVien,TongKetKy,Lop
+		where SinhVien.IDSinhVien=TongKetKy.IDSinhVien  and Lop.IDLop=SinhVien.IDLop
+		group by  SinhVien.HoTen,SinhVien.IDSinhVien,TenLop  ,DiemTBC,SoTCDKi,SoTCNo
+		 
+		go
+	--b2.Lấy ra 3 sinh viên điểm cao nhất
+		go
+		print(N'danh sách 3 sinh vien của lớp CNTT')
+		select top(3)HoTen as 'Họ và tên',IDSinhVien as 'Mã sinh viên',DiemTBC as 'Điểm trung bình trung',SoTCDKi as 'Số tín chỉ đăng ký',SoTCNo as 'Số tín chỉ nợ'
+		from DanhSachSinhVienCoDiemCaoNhatTrongLopChuyenNganh
+		where TenLop =N'CNTT16B'
+		go
+--2.3(view) .đưa ra top 3 sinh viên có điểm cao nhất của 1 khoa CNTT
+	--b1.Tạo view hiện thị danh sách kết quả của các khoa
+		go
+		CREATE VIEW DanhSachSinhVienCoDiemCaoNhatTrongKhoa AS
+		select SinhVien.HoTen ,SinhVien.IDSinhVien,TenKhoa ,DiemTBC,SoTCDKi,SoTCNo
+		from SinhVien,TongKetKy,Khoa,Lop
+		where SinhVien.IDSinhVien=TongKetKy.IDSinhVien  and Khoa.IDKhoa=Lop.IDKhoa and SinhVien.IDSinhVien=Lop.IDLop
+		group by  SinhVien.HoTen,SinhVien.IDSinhVien,TenKhoa  ,DiemTBC,SoTCDKi,SoTCNo
+		 
+		go
+	--b2.Lấy ra 3 sinh viên điểm cao nhất
+		go
+		print(N'danh sách 3 sinh vien của lớp CNTT')
+		select top(3)HoTen as 'Họ và tên',IDSinhVien as 'Mã sinh viên',DiemTBC as 'Điểm trung bình trung',SoTCDKi as 'Số tín chỉ đăng ký',SoTCNo as 'Số tín chỉ nợ'
+		from DanhSachSinhVienCoDiemCaoNhatTrongKhoa
+		 where TenKhoa = N'CNTT'
+		go
+--3.1 tạo proc lấy ra các sinh viên có chỗ ở hiện tại là @choOHienTai
+	--b1:Tạo proc 
+		go
+		CREATE PROC DanhSachSVC (@idLop char(10),@choOHienTai nvarchar(200))
+			AS
+			BEGIN
+				select 
+					SinhVien.IDSinhVien AS 'Mã sinh viên',
+					SinhVien.NgaySinh AS 'Ngày sinh',
+					SinhVien.KhoaDKi AS 'Khoa',
+					Lop.TenLop AS 'Lớp',
+					SinhVien.DiaChiHT as 'Chỗ ở hiệm tại'
+				from SinhVien, Lop
+				where 
+				SinhVien.DiaChiHT like (@choOHienTai )
+				and SinhVien.IDLop=Lop.IDLop
+				and Lop.IDLop like( @idLop)
+				end
+		go
+	--b2:Chạy proc
+		exec DanhSachSVC  1 ,N'Cầu Giấy'
+		DROP PROCEDURE DanhSachSVC
+
+----3.2. Thủ tục cập nhật điểm trung bình chung (điểm số) của một học phần bất kì
+go
+--3.2 tao proc lây ra thông tin các học phần sinh vien có @id tham gia
+	--b1:Tạo proc 
+		go
+		CREATE PROC DanhSachCacHocPhanCuaSinhVien (@idSv char(10))
+			AS
+			BEGIN
+				select 
+					SinhVien.HoTen as 'Tên sinh viên',
+					TenHocPhan as 'Tên học phần',
+					KetQuaHP.DiemCC as 'Điểm chuyên cần',
+					KetQuaHP.DiemTX as 'Điểm thường xuyên',
+					KetQuaHP.DiemThi as 'Điểm thi',
+					KetQuaHP.DiemHe10 as'Điểm tổng kết'
+				from SinhVien,HocPhan,KetQuaHP
+				where SinhVien.IDSinhVien=KetQuaHP.IDSinhVien and KetQuaHP.IDHocPhan=HocPhan.IDHocPhan
+				and SinhVien.IDSinhVien=@idSv
+				end
+		go
+	--b2:Chạy proc
+		exec DanhSachCacHocPhanCuaSinhVien 3
+
+----3.3. Tạo proc lấy ra các sinh viên có điểm tổng kết cao nhất trường của học kỳ @idHocky
+	--b1:Tạo proc
+		go
+		CREATE PROC ThongTinSinhVienCoDiemCaoNhatTruong (@idHK char(10))
+		as
+		begin
+		select top 1 with ties
+		SinhVien.IDSinhVien as 'Mã sinh viên',
+		 SinhVien.HoTen as 'Họ tên sinh viên',
+		Lop.TenLop as 'Tên lớp',
+		SinhVien.KhoaDKi as 'Tên khoa',
+		TongKetKy.DiemTBC as 'Điểm tổng kết chung'
+		from SinhVien,TongKetKy,HocKy,Lop
+		where SinhVien.IDSinhVien=TongKetKy.IDSinhVien and HocKy.IDHocKy=TongKetKy.IDHocKy and SinhVien.IDLop=Lop.IDLop and  HocKy.IDHocKy=@idHK
+		order by [Điểm tổng kết chung] DESC
+		end
+		go
+	--b2:Chạy
+		exec ThongTinSinhVienCoDiemCaoNhatTruong 1
+		go
+
+--4.Trigger
+	--tạo view hiển thị tổng sinh viên trong trường,lớp
+	go
+	create view TongSoSvTrongTruong
+	as
+		Select COUNT (t.[Tổng sinh viên trong truong]) as 'STT',t.[Tổng sinh viên trong truong]
+		from (select count(SinhVien.IDSinhVien) as 'Tổng sinh viên trong truong' from SinhVien) as t
+		group by t.[Tổng sinh viên trong truong]
+		go
+		
+	create view TongSoSvTrongLopChuyenNganh
+	as	
+		Select SinhVien.IDLop as'Mã lớp',TenLop as 'Tên lơp',
+		count(SinhVien.IDSinhVien) as 'Tổng sinh viên trong lớp'
+		from SinhVien,Lop
+		where SinhVien.IDLop=Lop.IDLop
+		group by Lop.TenLop,SinhVien.IDLop
+		go
+--4.1 tạo trigger cập nhật tự động tăng số lượng sv trong trường, trong lớp mỗi khi thêm sinh viên mới vào
+	create trigger CapNhatSLSVTang on SinhVien after insert as
+		begin
+			update TongSoSvTrongTruong
+			set [Tổng sinh viên trong truong]= (select count(IDSinhVien)  from inserted where STT=1)
+			from TongSoSvTrongTruong
+			
+			update TongSoSvTrongLopChuyenNganh
+			set [Tổng sinh viên trong lớp]=(select count(IDSinhVien)  from inserted where inserted.IDLop=TongSoSvTrongLopChuyenNganh.[Mã lớp])
+			from TongSoSvTrongLopChuyenNganh
+			join inserted on TongSoSvTrongLopChuyenNganh.[Mã lớp]=inserted.IDLop
+		end
+		go
+		DROP TRIGGER CapNhatSLSVTang
+		go
+--4.2tạo trigger cập nhật tự động số lượng sv trong trường và sinh viên 
+		create trigger CapNhatSLSVGiam on SinhVien after delete as
+		begin
+			update TongSoSvTrongTruong
+			set [Tổng sinh viên trong truong]= (select count(IDSinhVien)  from deleted where STT=1)
+			from TongSoSvTrongTruong
+			
+			update TongSoSvTrongLopChuyenNganh
+			set [Tổng sinh viên trong lớp]=(select count(IDSinhVien)  from deleted where deleted.IDLop=TongSoSvTrongLopChuyenNganh.[Mã lớp])
+			from TongSoSvTrongLopChuyenNganh
+			join deleted on TongSoSvTrongLopChuyenNganh.[Mã lớp]=deleted.IDLop
+		end
+		go
+		DROP TRIGGER CapNhatSLSVGiam
+		go
+--4.3tạo trigger cập nhật ghi chú null
+	create trigger CapNhatGhiChuNull on SinhVien after insert as
+		begin
+			update SinhVien
+			set GhiChu=N'không có gì'
+			from SinhVien
+			where GhiChu is null
+		end
+		go
+		DROP TRIGGER CapNhatGhiChuNull
+--5.Biểu mẫu đưa ra thông tin sinh viên
+
+-----------------------------------
+go
+drop proc bieumau_ds_sv_toan_truong
+go
+alter proc bieumau_ds_sv_toan_truong
+as
+begin
+	Declare
+		@STT char(5), 
+		@masv char(5), 
+		@hoten nvarchar(20), 
+		@ngaysinh date,
+		@GioiTinh nvarchar(5),
+		@QueQuan nvarchar(30), 
+		@DiaChiHienTai nvarchar(30),
+		@Khoa nvarchar(20), 
+		@TenLop nvarchar(40),
+		@GhiChu nvarchar(30),
+		@DAY nvarchar(2),
+		@YEAR nvarchar(4),
+		@MONTH nvarchar(2)
+
+	SET @DAY = (select DAY(GETDATE()))
+	SET @YEAR = (SELECT YEAR(GETDATE()))
+	SET @MONTH = ( Select MONTH(GETDATE()))
+	DECLARE cur_sor CURSOR GLOBAL FOR
+	select
+			SinhVien.IDSinhVien, 
+			SinhVien.HoTen,
+			SinhVien.NgaySinh,
+			SinhVien.GioiTinh,
+			SinhVien.QueQuan,
+			SinhVien.DiaChiHT,
+			SinhVien.KhoaDKi,
+			Lop.TenLop,
+			SinhVien.GhiChu
+	from SinhVien, Lop
+	where SinhVien.IDLop= Lop.IDLop 
+	and HoTen is not null
+	and IDSinhVien is not null
+	OPEN cur_sor     --mở con trỏ
+	print N' Học Viện Kỹ Thuật Quân Sự													        '
+	print N' Phòng Đào Tạo																	    ' 
+	PRINT N''
+	print N'								       DANH SÁCH SINH VIÊN TOAN TRUONG' 
+	FETCH NEXT FROM cur_sor     -- Đọc dòng đầu tiên
+	INTO  @masv,@hoten, @ngaysinh, @GioiTinh, @QueQuan, @DiaChiHienTai, @Khoa, @TenLop, @GhiChu
+	PRINT N''
+	PRINT N' ___________________________________________________________________________________________________________________________________________________________________________________'
+	print N'| STT |Mã SV|       Họ Tên       |     Ngày Sinh      | Giới tính |      Quê quán      |  Địa chỉ hiện tại  |        Khoa        |               Lớp            |      Ghi chú      |'
+	PRINT N' -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'
+
+	set @STT=1
+	WHILE (@@FETCH_STATUS = 0 )         
+	BEGIN
+		--in kết quả
+	--PRINT N'________________________________________________________________________________________________________________________________________________________________________________________________________________'
+			PRINT '|'+  CAST(@STT as nchar(5)) + '|' + CAST(@masv as nchar(5))+ '|' +cast(@hoten as nchar(20))+'|' +(cast(@ngaysinh as nchar(20))) + '|' + cast(@GioiTinh as nchar(11)) + '|' + cast(@QueQuan as nchar(20)) + '|' + cast(@DiaChiHienTai as nchar(20)) + '|'+ cast(@Khoa as nchar(20))+ '|'+ cast(@TenLop as nchar(30))+ '|'+cast(@GhiChu as nchar(20))+ '|'
+			if @@FETCH_STATUS <> 0 
+				break
+			set @STT=@STT+1
+			FETCH NEXT FROM cur_sor -- Đọc dòng tiếp
+				  INTO @masv,@hoten, @ngaysinh, @GioiTinh, @QueQuan, @DiaChiHienTai, @Khoa, @TenLop, @GhiChu
+		END	
+	PRINT N' ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------'
+	print N'                                                                                                                  Hà Nội, Ngày ' + @DAY + N' tháng ' + @MONTH + N' năm ' + @YEAR
+	print N'                                                                                                                                NGƯỜI LẬP BIỂU MẪU'
+	PRINT N'                                                                                                                              (ký và ghi rõ họ tên)'
+	PRINT N''
+	PRINT N''
+	PRINT N''
+	PRINT N'                                                                                                                               Nguyễn Quang Linh'
+		CLOSE cur_sor              -- Đóng Cursor
+		DEALLOCATE cur_sor			-- giải phóng tài nguyên
+end 
+	go
+--in ra biểu mẫu
+exec bieumau_ds_sv_toan_truong 
